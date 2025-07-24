3950241fecebbcd186e8e3714c67ebb84bb84611
Hassnat58 <117041126+Hassnat58@users.noreply.github.com>
2025-07-25 02:34:30 +0500

    Update msalConfig.ts

--- src/taskpane/components/msalConfig.ts
+++ src/taskpane/components/msalConfig.ts
@@ -2,9 +2,9 @@ import { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "edc31410-d406-476a-8ac1-b062e1df4a77", // Replace with your App Registration client ID
+    clientId: "a8f0615d-19b4-4c1d-b578-3f754d19e56c", 
     authority: "https://login.microsoftonline.com/common",
-    redirectUri: "https://5jd7y6.sharepoint.com/sites/HScommunication/Outlook",
+    redirectUri: "https://hassnat58.github.io/OutlookAddin",
   },
   cache: {
     cacheLocation: "localStorage",
